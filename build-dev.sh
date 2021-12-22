6fc60f3d4c37f3573b52982571177119d508d3c1
meziaris <meziaris@gmail.com>
2021-12-23 00:36:52 +0700

    improve docker build and push

--- build-dev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-docker build -t debio_frontend_revamp_dev .
-docker tag debio_frontend_revamp_dev hub.debio.network/debio_frontend_revamp_dev:latest
-docker push hub.debio.network/debio_frontend_revamp_dev:latest
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-docker build -t debio_frontend_revamp_service .
-docker tag debio_frontend_revamp_service hub.theapps.dev/debio_frontend_revamp_service:latest
-docker push hub.theapps.dev/debio_frontend_revamp_service:latest
